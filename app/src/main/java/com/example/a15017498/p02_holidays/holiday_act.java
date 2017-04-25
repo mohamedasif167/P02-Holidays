f6a230f8b94d77d359b0a085b5c08d6cd29129f0
mohamedasif167 <mohamedasif167@gmail.com>
2017-04-25 15:41:52 +0800

    Edited with a new way of adding in the list

--- app/src/main/java/com/example/a15017498/p02_holidays/holiday_act.java
+++ app/src/main/java/com/example/a15017498/p02_holidays/holiday_act.java
@@ -17,7 +17,6 @@ public class holiday_act extends AppCompatActivity {
     ListView lvHoliday;
     TextView TxtHoliday;
     ArrayList<holiday> holidaysSecular = new ArrayList<>();
-    ArrayList<holiday> holidaysReligion = new ArrayList<>();
     ArrayAdapter arrayAdapter;
 
     @Override
@@ -33,39 +32,54 @@ public class holiday_act extends AppCompatActivity {
 
         TxtHoliday.setText(category_name);
 
-        holidaysSecular.add(new holiday("New Year","1 Jan 2017","new_year","secular"));
-        holidaysSecular.add(new holiday("Chinese New Year","28-30 Jan 2017","cny","secular"));
-        holidaysSecular.add(new holiday("National Day","9 August 2017","national_day","secular"));
-
-        holidaysReligion.add(new holiday("Vesak Day","10 May 2017","vesak_day","religion"));
-        holidaysReligion.add(new holiday("Good Friday","14 April 2017","good_friday","religion"));
-        holidaysReligion.add(new holiday("Hari Raya ","25 June 2017","hari_raya_puasa","religion"));
-        holidaysReligion.add(new holiday("Hari Raya Haji","1 Septemember 2017","hari_raya_haji","religion"));
-        holidaysReligion.add(new holiday("Christmas Day","25 December 2017","christmas","religion"));
-
-        if(category_name.equalsIgnoreCase("Ethnic & Religion")){
-            arrayAdapter = new HolidayAdapter(this, R.layout.row, holidaysReligion);
-
-            lvHoliday.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    Toast.makeText(holiday_act.this,holidaysReligion.get(position).getName() +" Date: "+holidaysReligion.get(position).getDate(), Toast.LENGTH_SHORT).show();
-
-                }
-            });
+        if(category_name.equalsIgnoreCase("Secular")){
+            holidaysSecular.add(new holiday("New Year","1 Jan 2017","new_year","Happy New Year!, This celebration is to celebrated the new Year!"));
+            holidaysSecular.add(new holiday("Chinese New Year","28-30 Jan 2017","cny","Chinese Lunar New Year 2017 starts on a Saturday. As most businesses and organizations end work early on the eve, employees enjoy a three-and-a-half days long weekend break during this festive season. "));
+            holidaysSecular.add(new holiday("National Day","9 August 2017","national_day","Singapore celebrates its 52nd year of independence in 2017."));
 
         }else{
-            arrayAdapter = new HolidayAdapter(this, R.layout.row, holidaysSecular);
-
-            lvHoliday.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    Toast.makeText(holiday_act.this,holidaysSecular.get(position).getName() +" Date: "+holidaysSecular.get(position).getDate(), Toast.LENGTH_SHORT).show();
-
-                }
-            });
-
+            holidaysSecular.add(new holiday("Vesak Day","10 May 2017","vesak_day","Vesak Day is observed by Buddhists in Singapore, Thailand, Sri Lanka, Cambodia and many other southeast Asian countries in memory of Gautama Buddha's birth, attainment of Nirvana and passing."));
+            holidaysSecular.add(new holiday("Good Friday","14 April 2017","good_friday","Good Friday is the day Christians commemorate the crucifixion of Jesus Christ. It is the Friday before Easter Day (Sunday) which is itself not gazetted as a holiday in Singapore."));
+            holidaysSecular.add(new holiday("Hari Raya Puasa","25 June 2017","hari_raya_puasa","Hari Raya Puasa is the local Malay name for the Muslim religious holiday/celebration of Eid al-Fitr (or Aidil-fitri) which marks the end of the Islamic holy month of fasting,"));
+            holidaysSecular.add(new holiday("Hari Raya Haji","1 Septemember 2017","hari_raya_haji","Hari Raya Haji is the local Malay name for the Muslim religious holiday/celebration of Eid al-Adha. It is held to honour the willingness of the prophet Ibrahim(Abraham) to sacrifice his young son Ismail(Ishmael) in submission to Allah(God)."));
+            holidaysSecular.add(new holiday("Deepavali","18 October 2017","deepavali","Deepavali, also known as Diwali and, popularly, the Festival of Lights, is an Indian holiday/festival celebrated by Indians, especially by adherents of Hinduism, Sikhism and Jainism."));
+            holidaysSecular.add(new holiday("Christmas Day","25 December 2017","christmas","Singapore, with a substantial percentage of the population being of the Christian and Catholic faiths, celebrates this important day as a gazetted national holiday. "));
         }
+
+        arrayAdapter = new HolidayAdapter(this, R.layout.row, holidaysSecular);
+
+        lvHoliday.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(holiday_act.this,holidaysSecular.get(position).getCategory(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+//        if(category_name.equalsIgnoreCase("Ethnic & Religion")){
+//            //arrayAdapter = new HolidayAdapter(this, R.layout.row, holidaysReligion);
+//
+//            lvHoliday.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                @Override
+//                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                    Toast.makeText(holiday_act.this,holidaysReligion.get(position).getCategory(), Toast.LENGTH_SHORT).show();
+//
+//                }
+//            });
+//
+//        }else{
+//            arrayAdapter = new HolidayAdapter(this, R.layout.row, holidaysSecular);
+//
+//            lvHoliday.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                @Override
+//                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+////                    Toast.makeText(holiday_act.this,holidaysSecular.get(position).getName() +" Date: "+holidaysSecular.get(position).getDate(), Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(holiday_act.this,holidaysSecular.get(position).getCategory(), Toast.LENGTH_SHORT).show();
+//
+//                }
+//            });
+//
+//        }
 
         lvHoliday.setAdapter(arrayAdapter);
 
